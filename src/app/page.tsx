c26f46c27a22873d1cab6bc177795cb431c62eca
Kamil <kamil@iofinnet.com>
2025-09-19 14:30:35 +0400

    fix: title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import type { Timetable } from "@/types";
 import timelineData from "../../public/cedvel.json";
 import Table from "@/components/table";
 import { InstallPrompt, PushNotificationManager } from "@/components";
+import { TITLE } from "@/constants";
 
 const originalData = timelineData as any as Timetable;
 
@@ -12,8 +13,7 @@ export default function Home() {
       {/* <InstallPrompt /> */}
       <main className="flex flex-col items-center gap-4">
         <h1 className="w-full px-6 text-center text-2xl font-bold md:w-2/3">
-          Xaçmaz şəhəri Akademik Zərifə Əliyeva adına 8 nömrəli təbiət təmayüllü
-          liseyin rus bölməsi üzrə cədvəl (22 Sentyabr 2025)
+          {TITLE} (22 Sentyabr 2025)
         </h1>
 
         <Table originalData={originalData} />
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export const TITLE = `Xaçmaz şəhəri Akademik Zərifə Əliyeva adına 8 nömrəli təbiət təmayüllü liseyin rus bölməsi üzrə cədvəl`;
+export const TITLE = `Xaçmaz şəhəri Akademik Zərifə Əliyeva adına 8 nömrəli təbiət təmayüllü liseyin rus bölməsi üzrə cədvəli`;
 
 export const DOMAIN = "https://8nomreli.netlify.app";
